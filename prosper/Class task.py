52feaf28ca61630556897ffb27c1d708fae9c4f1
Progressive123 <ebereprosperc@gmail.com>
2026-04-16 14:55:52 +0100

    Class Task

---
+++ prosper/Class task.py
@@ -0,0 +1,12 @@
+list_of_fruits = ("Apple","Orange","Mango","Pear")
+print(list_of_fruits[2])
+
+set_of_even_numbers = {4,6,10,18,10,8,12}
+print(len(set_of_even_numbers))
+
+subjets = ["Physics", "Math", "Biology", "Chem", "Agric"]
+print("Total number of subjets is: ",len(subjets))
+
+students_age = {"John":13, "Peter":12, "Philip": 13, "Matther":13}
+print(students_age.keys())
+print(students_age.values())
