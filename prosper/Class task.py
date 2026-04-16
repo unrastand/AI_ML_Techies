3bfbf7cc01c19e37cc180f3ecf640f5e5d8f3c06
Progressive123 <ebereprosperc@gmail.com>
2026-04-16 15:02:19 +0100

    added comment

--- prosper/Class task.py
+++ prosper/Class task.py
@@ -1,12 +1,17 @@
+# Types of Data 1 : Tuple
+
 list_of_fruits = ("Apple","Orange","Mango","Pear")
 print(list_of_fruits[2])
 
+# Types of Data 1 : Set 
 set_of_even_numbers = {4,6,10,18,10,8,12}
 print(len(set_of_even_numbers))
 
+# Types of Data 1 : List
 subjets = ["Physics", "Math", "Biology", "Chem", "Agric"]
 print("Total number of subjets is: ",len(subjets))
 
+# Types of Data 1 : Dictionary
 students_age = {"John":13, "Peter":12, "Philip": 13, "Matther":13}
 print(students_age.keys())
 print(students_age.values())
