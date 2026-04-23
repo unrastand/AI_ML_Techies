5f47da3b06774c12e5dd94c8f1c7b134d0c32473
Progressive123 <ebereprosperc@gmail.com>
2026-04-23 15:20:09 +0100

    refine

--- prosper/Class take2.py
+++ prosper/Class take2.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-array_1D = np.arange(1,21)
+array_1D = np.random.randint(1,30,(1,21))
 
-matrix_2D = np.array([[2,4,5],[7,3,1],[6,4,9]])
+matrix_2D_first = np.array([[2,4,5],[7,3,1],[6,4,9]])
+matrix_2D_second = np.array([[2,4,5],[7,3,1],[6,4,9]])
+
+result_1 = np.add(matrix_2D_first,matrix_2D_second)
+result_2 = np.multiply(matrix_2D_first,matrix_2D_second)
 
 print(array_1D)
-print(matrix_2D)
+print(type(array_1D))
+#print(matrix_2D_first)
+print(result_1)
+print(result_2)
