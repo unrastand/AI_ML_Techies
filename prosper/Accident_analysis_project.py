e4cb66bf9ec1c0784a923c62ed2e409b6a4e6ff5
Progressive123 <ebereprosperc@gmail.com>
2026-05-06 14:18:17 +0100

    m

--- prosper/Accident_analysis_project.py
+++ prosper/Accident_analysis_project.py
@@ -33,6 +33,12 @@ cause_counts = dataset_df[causes_of_accidents].sum().sort_values(ascending=False
 
 correlation_with_deaths = dataset_df.corr(numeric_only=True)['Num_Killed'].sort_values(ascending=False)
 
+# States with the highest number of deaths in car accidents
+state_map = {1:'Abia', 2:'Adamawa', 3:'Akwa Ibom', 4:'Anambra', 5:'Bauchi', 6:'Bayelsa', 7:'Benue', 8:'Borno', 9:'Cross River', 10:'Delta', 11:'Ebonyi', 12:'Edo', 13:'Ekiti', 14:'Enugu', 15:'Gombe', 16:'Imo', 17:'Jigawa', 18:'Kaduna', 19:'Kano', 20:'Katsina', 21:'Kebbi', 22:'Kogi', 23:'Kwara', 24:'Lagos', 25:'Nasarawa', 26:'Niger', 27:'Ogun', 28:'Ondo', 29:'Osun', 30:'Oyo', 31:'Plateau', 32:'Rivers', 33:'Sokoto', 34:'Taraba', 35:'Yobe', 36:'Zamfara', 37:'FCT'}
+
+
+dataset_df['State'] = dataset_df['Num_Killed'].map(state_map)
+print(dataset_df['State'].value_counts())
 # Visualize the data
 plt.figure()
 sb.barplot(x=top_states_by_death.index, y=top_states_by_death.values)
@@ -46,4 +52,8 @@ plt.xticks(rotation=30)
 plt.title('Causes of Car Accidents')
 plt.xlabel('Cause')
 plt.ylabel('Count')
+plt.show()
+
+plt.pie(state_map.values, labels=label, autopct='%1.1f%%')
+plt.title('Death distribution by state')
 plt.show()
